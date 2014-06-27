22f0e28a11244cdd809615f1b3f3f6437ea06457
Reid Belton <belton.reid@gmail.com>
2014-06-26 21:29:01 -0700

    Initial Commit

---
+++ Project2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by reidbelton on 6/26/14.
+//  Copyright (c) 2014 Reid Belton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
